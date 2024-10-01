94b791f7b6905622fecbc6f36c84775c8b265e06
frederick-hernandez <53789782+frederick-hernandez@users.noreply.github.com>
2024-09-30 21:03:54 -0600

    problema

--- app/app.js
+++ app/app.js
@@ -4,12 +4,20 @@ const routerArea = require('./routers/area.router.js')
 const routerCliente = require('./routers/clientes.router.js')
 const routerproveedores = require('./routers/proveedores.router.js')
 const routerpcabecera = require('./routers/pcabecera.router.js')
+const cors = require('cors');
+app.use(cors());
 const app = express();
 app.get('/', (req, res) => {
     res.send('Welcome home')
 })
 
+const corsOptions = {
+  origin: 'https://ferreteria-api.onrender.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/v1',routeremp);
 app.use('/api/v2',routerArea);
